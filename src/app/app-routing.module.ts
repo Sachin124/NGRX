52f004909101e601e81413ec45bc86c74d8c5182
sachin <sachin@stackmint.com>
2019-05-17 18:30:19 +0530

    NGRX

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {} from "./customer/customer.module"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:HomeComponent},
+  {path: "customers",loadChildren:"./customer/customer.module#CustomerModule"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    // import the ngrx store module
+    StoreModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
